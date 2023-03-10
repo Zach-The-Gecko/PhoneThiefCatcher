7dece814c7c3ef5cb54c5d7afc2f8a1dc7f5f897
Zach Geckler <zach.the.gecko@gmail.com>
2023-03-10 11:07:14 -0800

    a

--- script.js
+++ script.js
@@ -15,48 +15,39 @@ permissionBtn.addEventListener("click", () => {
   DeviceMotionEvent.requestPermission();
 });
 
+const webcamElement = document.getElementById("webcam");
+const canvasElement = document.getElementById("canvas");
+const webcam = new Webcam(webcamElement, "user", canvasElement);
+
+const motionDetected = () => {
+  const alarmNoise = new Audio("./mixkit-alert-alarm-1005.mp3");
+  alarmNoise.loop = true;
+  alarmNoise.play();
+
+  webcam
+    .start()
+    .then((result) => {
+      console.log("Webcam Started!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  const loop = () => {
+    const picture = webcam.snap();
+    console.log(picture);
+    setTimeout(loop, 100);
+  };
+  loop();
+};
+
 window.addEventListener("devicemotion", (event) => {
   const accelerationY = event.acceleration.y;
   const accelerationX = event.acceleration.x;
   const accelerationZ = event.acceleration.z;
 
   if (accelerationY > 3 || accelerationX > 3 || accelerationZ > 3) {
-    console.log("MOVEMENT!!!!");
+    motionDetected();
+    window.removeEventListener("devicemotion");
   }
-});
-
-const motionDetected = () => {
-  const alarmNoise = new Audio("./mixkit-alert-alarm-1005.mp3");
-  alarmNoise.loop = true;
-  alarmNoise.play();
-};
-
-// setTimeout(motionDetected, 1000);
-
-const webcamElement = document.getElementById("webcam");
-const canvasElement = document.getElementById("canvas");
-const snapSoundElement = document.getElementById("snapSound");
-const webcam = new Webcam(
-  webcamElement,
-  "user",
-  canvasElement,
-  snapSoundElement
-);
-
-webcam
-  .start()
-  .then((result) => {
-    console.log("Taking picture!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-let picture = webcam.snap();
-
-const takePictureBtn = document.querySelector("#take-picture");
-
-takePictureBtn.addEventListener("click", () => {
-  const picture = webcam.snap();
-  console.log(picture);
 });
