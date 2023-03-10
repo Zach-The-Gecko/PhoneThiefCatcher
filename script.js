d885a828724b03df4b2476f6b2970a535760b42d
Zach Geckler <zach.the.gecko@gmail.com>
2023-03-10 10:14:00 -0800

    A

--- script.js
+++ script.js
@@ -6,10 +6,21 @@ const setDisplay = (text) => {
 
 const permissionBtn = document.querySelector("#permission");
 
+// if (location.protocol != "https:") {
+// location.href =
+//   "https:" + window.location.href.substring(window.location.protocol.length);
+// }
+
 permissionBtn.addEventListener("click", () => {
   DeviceMotionEvent.requestPermission();
 });
 
 window.addEventListener("devicemotion", (event) => {
-  console.log(event.acceleration);
+  const accelerationY = event.acceleration.y;
+  const accelerationX = event.acceleration.x;
+  const accelerationZ = event.acceleration.z;
+
+  if (accelerationY > 3 || accelerationX > 3 || accelerationZ > 3) {
+    console.log("MOVEMENT!!!!");
+  }
 });
