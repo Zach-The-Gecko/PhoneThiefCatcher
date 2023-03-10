f4a1793260eb8d7c84862a1350bd14f61f853d9a
Zach Geckler <zach.the.gecko@gmail.com>
2023-03-10 10:56:26 -0800

    a

--- script.js
+++ script.js
@@ -24,3 +24,39 @@ window.addEventListener("devicemotion", (event) => {
     console.log("MOVEMENT!!!!");
   }
 });
+
+const motionDetected = () => {
+  const alarmNoise = new Audio("./mixkit-alert-alarm-1005.mp3");
+  alarmNoise.loop = true;
+  alarmNoise.play();
+};
+
+// setTimeout(motionDetected, 1000);
+
+const webcamElement = document.getElementById("webcam");
+const canvasElement = document.getElementById("canvas");
+const snapSoundElement = document.getElementById("snapSound");
+const webcam = new Webcam(
+  webcamElement,
+  "user",
+  canvasElement,
+  snapSoundElement
+);
+
+webcam
+  .start()
+  .then((result) => {
+    console.log("Taking picture!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+let picture = webcam.snap();
+
+const takePictureBtn = document.querySelector("#take-picture");
+
+takePictureBtn.addEventListener("click", () => {
+  const picture = webcam.snap();
+  console.log(picture);
+});
